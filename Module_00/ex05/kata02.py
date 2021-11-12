0b2da009fe7fa0c01406be3566d86465e61215c6
Cristina Fernández Bornay <cristinafernandezbornay@outlook.com>
2021-11-12 16:24:46 +0100

    MODIF(m_00 ex_05): kata02 removed <

--- Module_00/ex05/kata02.py
+++ Module_00/ex05/kata02.py
@@ -1,4 +1,4 @@
 if __name__ == "__main__":
     t = (3, 30, 2019, 9, 25)
     h, m, year, month, day = t
-    print("{:0>2d}/{:0>2d}/{} {:0>2d}:{:0>2d}".format(month, day, year, h, m))
+    print("{:02d}/{:02d}/{} {:02d}:{:02d}".format(month, day, year, h, m))
