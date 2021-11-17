ee96cbbf28ff0addad587a213bfd253ca97ac3d5
Emilie Pham <epham@student.42.fr>
2021-11-17 11:03:33 +0100

    ADDING module_00/ex10/loading.py WORKING

--- Module_00/ex10/loading.py
+++ Module_00/ex10/loading.py
@@ -7,14 +7,20 @@ def ft_progress(lst):
                 ' [',
                 progressbar.Bar(marker='=',left='=',right='', marker_wrap=('','>')),
                 '] ',
+                progressbar.Counter(format='%(value)d/{}'.format(len(lst))),' ',
                 progressbar.Timer(format= 'elapsed time: %(elapsed)s')
             ]
     
     bar = progressbar.ProgressBar(max_value=len(lst), widgets=widgets).start()
     for i in range(len(lst)):
-        yield bar.update(i)
+        bar.update(i)
+        yield i*i
 
 ret = 0
-for elem in ft_progress(range(100)):
+X = range(100, 200)
+listy = X
+for elem in ft_progress(listy):
+    ret += (elem + 3) % 5
     time.sleep(0.1)
-    print(elem)
+print()
+print(ret)
