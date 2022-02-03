11849e4e79425cfb5bb7a349822cc7ee8e781ee9
Cristina Fernández Bornay <cristinafernandezbornay@outlook.com>
2022-02-03 22:58:28 +0100

    MODIFIED (m01 ex05): changed tests

--- Module_01/ex05/test.py
+++ Module_01/ex05/test.py
@@ -2,30 +2,37 @@ from the_bank import Account, Bank
 
 if __name__ == "__main__":
     bank = Bank()
+    print("==> Creating corrupted account [Even number of args]")
     smith_jane = Account(
         'Smith Jane',
         zip='911-745',
         value=1000.0,
         ref='1044618427ff2782f0bbece0abd05f31'
     )
+    bank.add(smith_jane)
     bank.fix_account(smith_jane)
+    bank.add(smith_jane)
 
+    print("\n==> Creating corrupted account [Arg starting by 'b']")
     william_john = Account(
         'William John',
         zip='100-064',
         value=6460.0,
         ref='58ba2b9954cd278eda8a84147ca73c87',
-        info=None
+        bref="lol",
     )
+    bank.add(william_john)
     bank.fix_account(william_john)
-
-    bank.add(smith_jane)
     bank.add(william_john)
 
-    if bank.transfer('William John', 'Smith Jane', 1000.0) is False:
+    print("\n==> Trying invalid transfer")
+    if bank.transfer('William John', 'Smith Jane', 54500.0) is False:
         print('Failed')
+    else:
+        print('Success')
 
-    if bank.transfer('William John', 'Smith Jane', 1000.0) is False:
+    print("\n==> Trying valid transfer")
+    if bank.transfer('William John', 'Smith Jane', 40.0) is False:
         print('Failed')
     else:
         print('Success')
