9ef21c6fac75404f305b9edf83aa9897c42aa0a4
Cristina Fernández Bornay <cristinafernandezbornay@outlook.com>
2022-02-03 23:37:27 +0100

    MODIFIED (m01 ex05): adding more tests

--- Module_01/ex05/test.py
+++ Module_01/ex05/test.py
@@ -2,18 +2,10 @@ from the_bank import Account, Bank
 
 if __name__ == "__main__":
     bank = Bank()
-    print("==> Creating corrupted account [Even number of args]")
-    smith_jane = Account(
-        'Smith Jane',
-        zip='911-745',
-        value=1000.0,
-        ref='1044618427ff2782f0bbece0abd05f31'
-    )
-    bank.add(smith_jane)
-    bank.fix_account(smith_jane)
-    bank.add(smith_jane)
+    print("==> [Bank] Adding not an account.")
+    bank.add(1)
 
-    print("\n==> Creating corrupted account [Arg starting by 'b']")
+    print("\n==> [Bank] Adding a corrupted account.")
     william_john = Account(
         'William John',
         zip='100-064',
@@ -25,14 +17,35 @@ if __name__ == "__main__":
     bank.fix_account(william_john)
     bank.add(william_john)
 
-    print("\n==> Trying invalid transfer")
-    if bank.transfer('William John', 'Smith Jane', 54500.0) is False:
-        print('Failed')
-    else:
-        print('Success')
+    print("\n==> [Bank] Adding account with repeated name.")
+    william_john = Account(
+        'William John',
+        value=0.0,
+        zip='03540'
+    )
+    bank.add(william_john)
 
-    print("\n==> Trying valid transfer")
-    if bank.transfer('William John', 'Smith Jane', 40.0) is False:
-        print('Failed')
-    else:
-        print('Success')
+    print("\n==> [Bank] Adding a corrupted account.")
+    smith_jane = Account(
+        'Smith Jane',
+        zip='911-745',
+        value=1000.0,
+        ref='1044618427ff2782f0bbece0abd05f31'
+    )
+    bank.add(smith_jane)
+
+    print("\n==> [Transfer] Invalid because of the corrupted account")
+    print('Failed') if bank.transfer('William John', 'Smith Jane', 1) is False else print('Success')
+
+    bank.fix_account(smith_jane)
+    bank.add(smith_jane)
+
+    print("\n==> [Transfer] Invalid because of name not a string")
+    print('Failed') if bank.transfer(123, 'Smith Jane', 10) is False else print('Success')
+
+
+    print("\n==> [Transfer] Invalid because of the amount")
+    print('Failed') if bank.transfer('William John', 'Smith Jane', 54500.0) is False else print('Success')
+
+    print("\n==> [Transfer] Valid")
+    print('Failed') if bank.transfer('William John', 'Smith Jane', 40.0) is False else print('Success')
