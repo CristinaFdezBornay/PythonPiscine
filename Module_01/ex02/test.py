960916f4baa01626fac18a186d6455fb02f67e5d
Cristina Fernández Bornay <cristinafernandezbornay@outlook.com>
2022-02-03 19:48:04 +0100

    MODIFIED (m01 ex02): small modifs better reading

--- Module_01/ex02/test.py
+++ Module_01/ex02/test.py
@@ -8,6 +8,7 @@ def main():
     print("{0: <20}".format("v1.shape: "), v1.shape) # should be (4,1)
     print("{0: <20}".format("v1.T()"), v1.T()) # Output: Vector([0.0, 1.0, 2.0, 3.0])
     print("{0: <20}".format("v1.T().shape"), v1.T().shape) # Output: (1,4)
+    input("\n================================================ Press ENTER to continue ==>")
 
     # Example 2:
     print("\nInitializing row vector with values [0.0, 1.0, 2.0, 3.0]")
@@ -16,6 +17,7 @@ def main():
     print("{0: <20}".format("v2.shape: "), v2.shape) # should be (1,4)
     print("{0: <20}".format("v2.T(): "), v2.T()) # Output: Vector([[0.0], [1.0], [2.0], [3.0]])
     print("{0: <20}".format("v2.T().shape"), v2.T().shape) # Output: (4,1)
+    input("\n================================================ Press ENTER to continue ==>")
 
     # Example 3:
     print("\nInitializing column vector with size 10")
@@ -24,6 +26,7 @@ def main():
     print("{0: <20}".format("v3.shape: "), v3.shape)
     print("{0: <20}".format("v3.T(): "), v3.T())
     print("{0: <20}".format("v3.T().shape"), v3.T().shape)
+    input("\n================================================ Press ENTER to continue ==>")
 
     # Example 4:
     print("\nInitializing column vector with range (2, 9)")
@@ -32,13 +35,14 @@ def main():
     print("{0: <20}".format("v4.shape: "), v4.shape)
     print("{0: <20}".format("v4.T(): "), v4.T()) 
     print("{0: <20}".format("v4.T().shape"), v4.T().shape)
-
+    input("\n================================================ Press ENTER to continue ==>")
     # ADDITION
     print("\nVECTOR ADDITION")
     print("{0: <20}".format("v1: "), v1)
     print("{0: <20}".format("v2.T(): "), v2.T()) # Output: Vector([[0.0], [1.0], [2.0], [3.0]])
     print("{0: <20}".format("v1 + v2.T(): "), v1 + v2.T())
     print("{0: <20}".format("(v1 + v2.T()).shape: "), (v1 + v2.T()).shape)
+    input("\n================================================ Press ENTER to continue ==>")
 
     # SUBSTRACTION
     print("\nVECTOR SUBSTRACTION")
@@ -46,12 +50,14 @@ def main():
     print("{0: <20}".format("v2.T(): "), v2.T()) # Output: Vector([[0.0], [1.0], [2.0], [3.0]])
     print("{0: <20}".format("v1 - v2.T(): "), v1 - v2.T())
     print("{0: <20}".format("(v1 - v2.T()).shape: "), (v1 - v2.T()).shape)
+    input("\n================================================ Press ENTER to continue ==>")
 
     # DIVISION
     print("\nVECTOR DIVISION BY SCALAR")
     print("{0: <20}".format("v1: "), v1)
     print("{0: <20}".format("v1 / 2: "), v1 / 2)
     print("{0: <20}".format("(v1 / 2).shape: "), (v1 / 2).shape)
+    input("\n================================================ Press ENTER to continue ==>")
 
     # Error management
     print("\n===========================ERROR MANAGEMENT=========================")
@@ -71,6 +77,7 @@ def main():
     print("\nInitializing vector with array of inconsistent types")
     v5 = Vector(["hello", 15, 0.5, None])
     print("{0: <20}".format("v5: "), v5)
+    input("\n================================================ Press ENTER to continue ==>")
 
     print("\nVector addition with scalar")
     print("{0: <20}".format("v1: "), v1)
@@ -78,7 +85,8 @@ def main():
 
     print("\nString addition with vector")
     print("{0: <20}".format("v1: "), v1)
-    print("{0: <20}".format("'hello' + v1: "), "hello"+v1)
+    print("{0: <20}".format("'hello' + v1: "), "hello" + v1)
+    input("\n================================================ Press ENTER to continue ==>")
 
     print("\nVector division by 0")
     print("{0: <20}".format("v1: "), v1)
