e31a44e51088f47395efbb37e74703b538dac5d9
Cristina Fernández Bornay <cristinafernandezbornay@outlook.com>
2022-02-20 16:42:13 +0100

    MODIFIED (m03 ex00): working perfffff + tests finished

--- Module_03/ex00/NumPyCreator.py
+++ Module_03/ex00/NumPyCreator.py
@@ -3,34 +3,57 @@ from typing import List, Iterable, Tuple
 
 class NumPyCreator():
     def from_list(lst:list=[]):
-        if type(lst) != list:
-            return None
-        for l in lst:
-            if len(l) != len(lst[0]):
-                return None
         try:
-            return np.array(lst, dtype='<U21')
+            if type(lst) != list:
+                return None
+            for l in lst:
+                if len(l) != len(lst[0]):
+                    return None
+            return np.array(lst)
         except:
             return None
     
-    def from_tuple(tpl:tuple=(42,42)):
-        if type(tpl) != tuple:
+    def from_tuple(tpl:tuple):
+        try:
+            if type(tpl) != tuple:
+                return None
+            return np.array(tpl)
+        except:
             return None
-        return np.array(tpl)
 
     def from_iterable(itr:iter):
-        lst:list = []
-        for it in itr:
-            lst.append(it)
-        return np.array(lst)
+        try:
+            lst:list = []
+            for it in itr:
+                lst.append(it)
+            return np.array(lst)
+        except:
+            return None
 
-    def from_shape(shape:Tuple=(4, 2), value:int=0):
-        output = np.ones(shape)
-        output *= value
-        return output
+    def from_shape(shape:tuple, value:int=0):
+        try:
+            if type(shape) != tuple:
+                return None
+            if type(value) != int:
+                return None
+            output = np.ones(shape)
+            output *= value
+            return output
+        except:
+            return None
     
-    def random(shape:Tuple=(4, 2)):
-        return np.random.randint(0, 100, shape)
+    def random(shape:tuple):
+        try:
+            if type(shape) != tuple:
+                return None
+            return np.random.randint(0, 100, shape)
+        except:
+            return None
 
-    def identity(n:int=3):
-        return np.identity(n)
+    def identity(n:int):
+        try:
+            if type(n) != int:
+                return None
+            return np.identity(n)
+        except:
+            return None
--- Module_03/ex00/test.py
+++ Module_03/ex00/test.py
@@ -1,38 +1,95 @@
 from NumPyCreator import NumPyCreator as npc
+import numpy as np
+import warnings
+warnings.filterwarnings("error")
 
-def print_expected_output(output):
-    print("=> expected output:\n{}".format(output))
+functions_tests = [
+    {
+        "ft_name": "npc.from_list",
+        "ft_ours": npc.from_list,
+        "ft_real": np.array,
+        "test": [
+            [[1,2,3], [6,3,4]],
+            [['1','2','3'],['a','b','c'],['5.0', '6.1', '7.8']],
+            [[],[]],
+            None,
+            4,
+            [[1,2,3],[6,4]],
+            ((1,2),(3,4))
+        ]
+    },
+    {
+        "ft_name": "npc.from_tuple",
+        "ft_ours": npc.from_tuple,
+        "ft_real": np.array,
+        "test": [
+            (0, 10),
+            ("a","b","c"),
+            (1, 4, 6),
+            ((1,2),(3,4)),
+            None,
+            4,
+            [[1,2,3],[6,3,4]]
+        ]
+    },
+    {
+        "ft_name": "npc.from_iterable",
+        "ft_ours": npc.from_iterable,
+        "ft_real": np.array,
+        "test": [
+            range(5),
+            range(23, 28),
+            None,
+            4,
+            [[1,2,3],[6,3,4]]
+        ]
+    },
+    {
+        "ft_name": "npc.from_shape",
+        "ft_ours": npc.from_shape,
+        "ft_real": np.zeros,
+        "test": [
+            (3,5),
+            (0,0),
+            None,
+            4,
+            [[1,2,3],[6,3,4]]
+        ]
+    },
+    {
+        "ft_name": "npc.random",
+        "ft_ours": npc.random,
+        "ft_real": None,
+        "test": [
+            (3,5),
+            None,
+            4,
+            [[1,2,3],[6,3,4]]
+        ]
+    },
+    {
+        "ft_name": "npc.identity",
+        "ft_ours": npc.identity,
+        "ft_real": np.identity,
+        "test": [
+            0,
+            4,
+            None,
+            [[1,2,3],[6,3,4]]
+        ]
+    },
+]
 
 if __name__=="__main__":
-    print("==== FROM_LIST 1 ====")
-    print_expected_output("array([[1,2,3],\n[6,3,4]])")
-    print("=> real output:\n{}".format(npc.from_list([[1,2,3],[6,3,4]])))
-    lol = npc.from_list([[1,2,3],[6,3,4]])
-    print("\n==== FROM_LIST 2 ====")
-    print_expected_output("None")
-    print("=> real output:\n{}".format(npc.from_list([[1,2,3],[6,4]])))
-    print("\n==== FROM_LIST 3 ====")
-    print_expected_output("array(['1', '2', '3'],\n['a', 'b', 'c'],\n['5.0', '6.1', '7.8']], dtype='<U21')")
-    print("=> real output:\n{}".format(npc.from_list([[1,2,3],['a','b','c'],[6,4,7]])))
-    print("\n==== FROM_LIST 4 ====")
-    print_expected_output("None")
-    print("=> real output:\n{}".format(npc.from_list(((1,2),(3,4)))))
-    print("\n==== FROM_TUPLE 1 ====")
-    print_expected_output("array([['a','b','c'])")
-    print("=> real output:\n{}".format(npc.from_tuple(("a","b","c"))))
-    print("\n==== FROM_TUPLE 2 ====")
-    print_expected_output("None")
-    print("=> real output:\n{}".format(npc.from_tuple([[1,2,3],[6,3,4]])))
-    print("\n==== FROM_ITERABLE ====")
-    print_expected_output("array([0, 1, 2, 3, 4])")
-    print("=> real output:\n{}".format(npc.from_iterable(range(5))))
-    print("\n==== FROM_SHAPE ====")
-    shape=(3,5)
-    print_expected_output("array([[0, 0, 0, 0, 0],\n[0, 0, 0, 0, 0],\n[0, 0, 0, 0, 0]])")
-    print("=> real output:\n{}".format(npc.from_shape(shape)))
-    print("\n==== RANDOM ====")
-    print_expected_output("array([[rand, rand, rand, rand, rand ],\n[rand, rand, rand, rand, rand],\n[rand, rand, rand, rand, rand]])")
-    print("=> real output:\n{}".format(npc.random(shape)))
-    print("\n==== IDENTITY ====")
-    print_expected_output("array([[1., 0., 0., 0.],\n[0., 1., 0., 0.],\n[0., 0., 1., 0.],\n[0., 0., 0., 1.]])")
-    print("=> real output:\n{}".format(npc.identity(4)))
+    for ft_test in functions_tests:
+        print(f"\n==== TESTING ===> {ft_test['ft_name']}")
+        for test in ft_test['test']:
+            print(f"=== Test {ft_test['ft_name']} : {str(test)}")
+            try:
+                if ft_test['ft_real']:
+                    print(f"= Numpy output :\n{ft_test['ft_real'](test)}")
+            except:
+                print(f"= Numpy crashed when testing {str(ft_test['ft_real'])}({str(test)})")
+                pass
+            print(f"= Our output :\n{ft_test['ft_ours'](test)}")
+            input("====>\n")
