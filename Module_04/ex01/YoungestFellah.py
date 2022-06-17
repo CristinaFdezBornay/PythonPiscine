28f53adc973fe97956cd54129921d09bcf8fe3dc
Cristina Fernández Bornay <cristinafernandezbornay@outlook.com>
2022-06-17 18:54:51 +0200

    MODIFY (m04 ex01): check if data for the year

--- Module_04/ex01/YoungestFellah.py
+++ Module_04/ex01/YoungestFellah.py
@@ -15,12 +15,13 @@ def youngestFellah(df, year):
             raise ValueError("Invalid year value. Argument year should be an int.")
         if not isinstance(df, pd.DataFrame):
             raise ValueError("Invalid df value. Argument df should be a pandas.DataFrame object.")
-        else:
-            selected_year = df[df['Year'] == year]
-            female_athletes = selected_year[selected_year['Sex'] == 'F']
-            male_athletes = selected_year[selected_year['Sex'] == 'M']
-            youngest_dict['f'] = female_athletes.min()['Age']
-            youngest_dict['m'] = male_athletes.min()['Age']
+        selected_year = df[df['Year'] == year]
+        if len(selected_year) == 0:
+            raise ValueError("No data for the provided year.")
+        female_athletes = selected_year[selected_year['Sex'] == 'F']
+        male_athletes = selected_year[selected_year['Sex'] == 'M']
+        youngest_dict['f'] = female_athletes.min()['Age']
+        youngest_dict['m'] = male_athletes.min()['Age']
     except ValueError as e:
         print("Error: {}".format(e))
     return youngest_dict
