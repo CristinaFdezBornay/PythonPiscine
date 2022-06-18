910a314be2ca966da630b9e0edcec271f517ea8f
Cristina Fernández Bornay <cristinafernandezbornay@outlook.com>
2022-06-18 04:03:45 +0200

    MODIFY (m04 ex05): no puedo maaaasssss

--- Module_04/ex06/MyPlotLib.py
+++ Module_04/ex06/MyPlotLib.py
@@ -1,7 +1,8 @@
 from numpy import isin
 import pandas as pd
 import matplotlib.pyplot as plt
-import scipy
+from scipy.stats import gaussian_kde
+import seaborn
 import numpy as np
 
 class MyPlotLib():
@@ -15,44 +16,48 @@ class MyPlotLib():
         """
         """
 
+    def _error_management_histogram(self, data, features):
+        if not isinstance(data, pd.DataFrame):
+            raise TypeError("Invalid df value. Argument df should be a pandas.DataFrame object.")
+        if not isinstance(features, list):
+            raise TypeError("Invalid features value. Argument features should be a list of features (str)")
+        for feature in features:
+            if not isinstance(feature, str):
+                raise TypeError(f"Invalid feature value. Feature {feature} should be a (str)")
+    
+    def _select_features_histogram(self, data, features):
+        selected_features = []
+        for feature in features:
+            if feature not in data.columns:
+                raise TypeError(f"Invalid feature value {feature}. Please make sure feature list is a valid column name.")
+            feature_data = data[feature].dropna()
+            if not all(isinstance(x, int) for x in feature_data) and not all(isinstance(x, float) for x in feature_data):
+                print(f"Cannot plot {feature} histogram, not a numerical value.")
+                continue
+            selected_features.append(feature)
+        return selected_features
+
     def histogram(self, data, features):
         """
         plots one histogram for each numerical feature in the list
         """
+
         try:
-            if not isinstance(data, pd.DataFrame):
-                raise TypeError("Invalid df value. Argument df should be a pandas.DataFrame object.")
-            if not isinstance(features, list):
-                raise TypeError("Invalid features value. Argument features should be a list of features (str)")
-            selected_features = []
-            for feature in features:
-                if feature not in data.columns:
-                    raise TypeError("Invalid feature value {}. Please make sure feature list is a valid column name.".format(feature))
-                feature_data = data[feature].dropna()
-                if not all(isinstance(x, int) for x in feature_data) and not all(isinstance(x, float) for x in feature_data):
-                    print("Cannot plot {} histogram, not a numerical value.".format(feature))
-                    continue
-                selected_features.append(feature)
+            self._error_management_histogram(data, features)
+            selected_features = self._select_features_histogram(data, features)
+            if len(selected_features) == 0:
+                raise Exception("None of the provided features can be plotted as an histogram.")
             fig, axes = plt.subplots(nrows=len(selected_features), ncols=1)
-            count = 0
-            data
-            for feature in selected_features:
-                if feature not in data.columns:
-                    raise ValueError("Invalid features value. Are you sure the feature you are trying to plot is in the dataframe?")
-                selected_data = data[feature]
-                print(selected_data.unique())
-                selected_data = selected_data.dropna()
-                selected_data = selected_data.sort_values()
+            for count, feature in enumerate(selected_features):
+                selected_data = data[feature].dropna().sort_values()
                 min = (round(selected_data.min()/10) - 1) * 10
                 max = (round(selected_data.min()/10) + 1) * 10
                 bin_number = max - min
-                print("Plotting {} hist, with {} bins".format(feature, bin_number))
+                print(f"Plotting {feature} hist, with {bin_number} bins")
                 plt.style.use('ggplot')
                 ax = axes if len(selected_features) == 1 else axes[count]
                 data.hist(column=feature, ax=ax, bins=bin_number)
-                count += 1
             plt.show()
-
         except Exception as e:
             print("Error: {}".format(e))
 
@@ -61,44 +66,22 @@ class MyPlotLib():
         plots the density curve of each numerical feature in the list
         """
         try:
-            if not isinstance(data, pd.DataFrame):
-                raise TypeError("Invalid df value. Argument df should be a pandas.DataFrame object.")
-            if not isinstance(features, list):
-                raise TypeError("Invalid features value. Argument features should be a list of features (str)")
-            
-            selected_features = []
-            for feature in features:
-                if feature not in data.columns:
-                    raise TypeError("Invalid feature value {}. Please make sure feature list is a valid column name.".format(feature))
-                feature_data = data[feature].dropna()
-                if not all(isinstance(x, int) for x in feature_data) and not all(isinstance(x, float) for x in feature_data):
-                    print("Cannot plot {} histogram, not a numerical value.".format(feature))
-                    continue
-                selected_features.append(feature)
-            fig, axes = plt.subplots(nrows=len(selected_features), ncols=1)
-            count = 0
-            data
-            for feature in selected_features:
-                if feature not in data.columns:
-                    raise ValueError("Invalid features value. Are you sure the feature you are trying to plot is in the dataframe?")
-                selected_data = data[feature]
-                print(selected_data.unique())
-                selected_data = selected_data.dropna()
-                selected_data = selected_data.sort_values()
-                # min = (round(selected_data.min()/10) - 1) * 10
-                # max = (round(selected_data.min()/10) + 1) * 10
-                # bin_number = max - min
-                print("Plotting {} density".format(feature))
-                # plt.style.use('ggplot')
-                # ax = axes if len(selected_features) == 1 else axes[count]
-                # data.hist(column=feature, ax=ax, bins=bin_number)
-                ax = selected_data.plot.kde()
-                count += 1
+            self._error_management_histogram(data, features)
+            selected_features = self._select_features_histogram(data, features)
+            if len(selected_features) == 0:
+                raise Exception("None of the provided features can be plotted as an histogram.")
+            plt.style.use('ggplot')
+            fig, axes = plt.subplots()
+            for count, feature in enumerate(selected_features):
+                selected_data = data[feature].dropna().sort_values()
+                density = gaussian_kde(selected_data)
+                xs = np.linspace(min(selected_data), max(selected_data), 200)
+                density.covariance_factor = lambda : .25
+                density._compute_covariance()
+                plt.plot(xs, density(xs))
             plt.show()
-
         except Exception as e:
             print("Error: {}".format(e))
-
 
     def pair_plot(self, data, features):
         """
@@ -106,8 +89,31 @@ class MyPlotLib():
         On each subplot shows a scatter plot of one numerical variable against another one. 
         The main diagonal of this matrix shows simple histograms.
         """
+        try:
+            self._error_management_histogram(data, features)
+            selected_features = self._select_features_histogram(data, features)
+            if len(selected_features) == 0:
+                raise Exception("None of the provided features can be plotted as an histogram.")
+            plt.style.use('ggplot')
+            selected_data = data[selected_features].dropna()
+            seaborn.pairplot(selected_data)
+            plt.show()
+        except Exception as e:
+            print("Error: {}".format(e))
 
     def box_plot(self, data, features):
         """
         displays a box plot for each numerical variable in the dataset.
         """
+        try:
+            self._error_management_histogram(data, features)
+            selected_features = self._select_features_histogram(data, features)
+            if len(selected_features) == 0:
+                raise Exception("None of the provided features can be plotted as an histogram.")
+            plt.style.use('ggplot')
+            selected_data = data[selected_features].dropna()
+            print(selected_data)
+            seaborn.boxplot(data=selected_data)
+            plt.show()
+        except Exception as e:
+            print("Error: {}".format(e))
--- Module_04/ex06/test.py
+++ Module_04/ex06/test.py
@@ -1,9 +1,43 @@
 from MyPlotLib import MyPlotLib
 import pandas as pd
 
-data = pd.read_csv('../data/athlete_events.csv')
+features_to_test = [
+    ['Age'],
+    ['Age', 'Weight'],
+    ['Age', 'Weight', 'City', 'Year'],
+    ['Age', 'Weight', 'LOOL', 'Year'],
+    ['Age', 'Weight', None, 'Year'],
+    None
+]
 
-myplot = MyPlotLib()
+if __name__=="__main__":
+    try:
+        file = '../data/athlete_events.csv'
+        print(f"\n=> Loading file")
+        data = pd.read_csv('../data/athlete_events.csv')
 
-# myplot.histogram(data, ['Age', 'Weight', 'City', 'Year'])
-myplot.density(data, ['Age', 'Weight', 'City', 'Year'])
+        myplot = MyPlotLib()
+
+        print(f"\n==> TESTING HISTOGRAM")
+        for test in features_to_test:
+            print(f"\n=> Test :{test}")
+            myplot.histogram(data, test)
+
+        print(f"\n==> TESTING DENSITY")
+        for test in features_to_test:
+            print(f"\n=> Test :{test}")
+            myplot.density(data, test)
+
+        print(f"\n==> TESTING PAIR PLOT")
+        for test in features_to_test:
+            print(f"\n=> Test :{test}")
+            myplot.pair_plot(data, test)
+
+        print(f"\n==> TESTING BOX PLOT")
+        for test in features_to_test:
+            print(f"\n=> Test :{test}")
+            myplot.box_plot(data, test)
+
+    except Exception as e:
+        print(e)
+
